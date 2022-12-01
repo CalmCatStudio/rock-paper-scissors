28a5ca9d0e963151efb2c711eef170158559e9c7
Kris Anderson <CalmCatStudio@gmail.com>
2022-11-30 20:05:25 -0500

    Finish MVP

--- javascript.js
+++ javascript.js
@@ -1,6 +1,90 @@
-function testComputerChoice()
+function play(playerChoice)
 {
-    alert(getComputerChoice())
+    let cpuChoice = getComputerChoice();
+    playerChoice = formatPlayerChoice(playerChoice);
+    if (playerChoice !== null)
+    {
+        console.log(GetWinner(cpuChoice, playerChoice));
+    }
+
+}
+
+function GetWinner(cpuChoice, playerChoice)
+{
+    let playerWins = false;
+    switch (playerChoice)
+    {
+        case "Rock":
+            switch (cpuChoice)
+            {
+                case "Rock":
+                    return declareDraw();
+                case "Paper":
+                    playerWins = false;
+                    break;
+                case "Scissors":
+                    playerWins = true;
+                    break;
+            }
+            break;
+        case "Paper":
+            switch (cpuChoice)
+            {
+                case "Rock":
+                    playerWins = true;
+                    break;
+                case "Paper":
+                    return declareDraw();                    
+                case "Scissors":
+                    playerWins = false;
+                    break;
+            }
+            break;
+        case "Scissors":
+            switch (cpuChoice)
+            {
+                case "Rock":
+                    playerWins = false;
+                    break;
+                case "Paper":
+                    playerWins = true;
+                    break;
+                case "Scissors":
+                    return declareDraw();
+            }
+            break;
+    }
+    if (playerWins)
+    {
+        return declareWinner("Player", playerChoice, "CPU", cpuChoice)
+    }
+    else
+    {
+        return declareWinner("CPU", cpuChoice, "Player", playerChoice)
+    }
+}
+
+function declareWinner(winner, winnerChoice, loser, loserChoice)
+{
+    return `${winner} beats ${loser}: ${winnerChoice} beats ${loserChoice}`;
+}
+
+function declareDraw()
+{
+    return "Draw";
+}
+
+function formatPlayerChoice(playerChoice)
+{
+    if (playerChoice && playerChoice.length >= 4)
+    {
+        playerChoice = playerChoice[0].toUpperCase() + playerChoice.substring(1).toLowerCase();
+        if (playerChoice === "Rock" || playerChoice === "Paper" || playerChoice === "Scissors")
+        {
+            return playerChoice;
+        }
+    }
+    return null;
 }
 
 function getComputerChoice()
