0ced457e4863d2a23446864e4aa5d6c6c9570d23
Kris Anderson <CalmCatStudio@gmail.com>
2022-11-30 19:09:34 -0500

    Add Computer guessing method

--- javascript.js
+++ javascript.js
@@ -1,4 +1,21 @@
-function test()
+function testComputerChoice()
 {
-    alert("Test complete");
+    alert(getComputerChoice())
+}
+
+function getComputerChoice()
+{
+    let choice = Math.floor(Math.random() * 3);
+    switch (choice)
+    {
+        case 0:
+            return "Rock";
+            
+        case 1:
+            return "Paper";
+        case 2: 
+        return "Scissors"
+        default:
+            return null;
+    }
 }
